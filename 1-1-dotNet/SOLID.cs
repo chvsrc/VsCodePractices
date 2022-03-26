808ef6d3f65f83a4b9028756ddac760b4b3c29fd
chvsrc <89882229+chvsrc@users.noreply.github.com>
2022-03-25 17:43:32 -0700

    Add files via upload

---
+++ 1-1-dotNet/SOLID.cs
@@ -0,0 +1,19 @@
+/*
+software entities :: classes, modules, functions, etc.
+
+S: Single Responsi-bility principle: 
+Every class should have only one responsibility.
+
+O: Open–closed principle: 
+software entities(classes) should be open for extension, but closed for modification.
+
+L: Lis-ko-v sub-sti-tution principle: 
+Functions that use references to base classes 
+must be able to use objects of derived classes without knowing it.
+
+I: Interface se-gre-ga-tion principle:
+Many client-specific interfaces are better than one general-purpose interface.
+
+D: Dependency inversion principle:
+Depend upon abstractions, [not] con-cre-tions.
+*/
