32a72515194072da0f93fea3e6ce25a4b8115e92
chvsrc <89882229+chvsrc@users.noreply.github.com>
2022-03-29 20:03:25 -0700

    Create ExtensionMethodClass.cs

---
+++ 1-1-dotNet/C-Sharp/ExtensionMethodClass.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Any Integer Value" + System.Environment.NewLine);
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            bool result = userInput.isGreaterThan5();
+            Console.WriteLine("The result is " + result);
+        }
+    }
+
+    // static class  
+    // static method 
+    // this as a method parameter
+    public static class ExtensionMethodClass
+    {
+        public static bool isGreaterThan5(this int input)
+        {
+            bool result = false;
+            if (input > 5)
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
