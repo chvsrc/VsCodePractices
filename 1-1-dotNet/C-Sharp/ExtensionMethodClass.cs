d527f749bfd057c7e0513320e68b524883077139
chvsrc <89882229+chvsrc@users.noreply.github.com>
2022-04-02 01:44:37 -0400

    Update ExtensionMethodClass.cs

--- 1-1-dotNet/C-Sharp/ExtensionMethodClass.cs
+++ 1-1-dotNet/C-Sharp/ExtensionMethodClass.cs
@@ -1,32 +1,39 @@
 using System;
 
-namespace Testing
+// when we get a DateTime value in string format from database
+// all the time we don't need to write this logic for validating the datetime
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Enter any date (12-21-2020):" + Environment.NewLine);
+        string userInput = Console.ReadLine();
+        if (userInput.IsValidDate())
         {
-            Console.WriteLine("Enter Any Integer Value" + System.Environment.NewLine);
-            int userInput = Convert.ToInt32(Console.ReadLine());
-            bool result = userInput.isGreaterThan5();
-            Console.WriteLine("The result is " + result);
+            Console.WriteLine(userInput + " is valid date");
+        }
+        else
+        {
+            Console.WriteLine(userInput + " is not a valid date");
         }
     }
+}
 
-    // static class  
-    // static method 
-    // this as a method parameter
-    // To convert one dto to another dto we can create a extension methods
-    public static class ExtensionMethodClass
+// static class  
+// static method 
+// this as a method parameter
+public static class ExtensionMethodClass
+{
+    public static bool IsValidDate(this string input)
     {
-        public static bool isGreaterThan5(this int input)
+        try
         {
-            bool result = false;
-            if (input > 5)
-            {
-                result = true;
-            }
-            return result;
+            DateTime.Parse(input);
+            return true;
+        }
+        catch
+        {
+            return false;
         }
     }
 }
