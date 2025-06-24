6ab0b8368ce1c5370725013824ce8cbdc9adec27
Venkat Ch <>
2025-06-24 19:54:10 -0400

    Updated CollectionsGenericsTest.cs

--- 1-1-dotNet/C-Sharp/CollectionsGenericsTest.cs
+++ 1-1-dotNet/C-Sharp/CollectionsGenericsTest.cs
@@ -36,19 +36,21 @@ public class GenericList<T>
 {
     public void Add(T input) { }
 }
+
 class CollectionsAndGenericTesting
 {
-   public static void Run()
-   {
+    public static void Run()
+    {
         #region Non-Generic-Collections
         #region ArrayList
         ArrayList al = new ArrayList();
         al.Add("Testing"); // string
         al.Add(7); //int
         al.Add(DateTime.Parse("8-oct-1985")); // DateTime
+        Console.WriteLine("ArrayListData :: " + al[0]);
         foreach (object o in al)
         {
-            Console.WriteLine("ArrayList Data :: " + o);
+            Console.WriteLine("ArrayListData :: " + o);
         }
         #endregion ArrayList
 
@@ -59,9 +61,10 @@ class CollectionsAndGenericTesting
         ht.Add("vb", 333);
         ht.Add("cs", "cs.net");
         ht.Add("asp", "asp.net");
+        Console.WriteLine("HashtableData :: " + ht["cs"]);
         foreach (DictionaryEntry d in ht)
         {
-            Console.WriteLine("Hashtable Data :: " + d.Key + " " + d.Value);
+            Console.WriteLine("HashtableData :: " + d.Key + " " + d.Value);
         }
         #endregion Hashtable
 
@@ -73,9 +76,10 @@ class CollectionsAndGenericTesting
         sl.Add("vb", 333);
         sl.Add("cs", "cs.net");
         sl.Add("asp", "asp.net");
+        Console.WriteLine("SortedListData :: " + sl["asp"]);
         foreach (DictionaryEntry d in sl)
         {
-            Console.WriteLine("SortedList Data :: " + d.Key + " " + d.Value);
+            Console.WriteLine("SortedListData :: " + d.Key + " " + d.Value);
         }
         #endregion SortedList
 
@@ -86,9 +90,10 @@ class CollectionsAndGenericTesting
         stk.Push(DateTime.Parse("8-oct-1985"));
         stk.Push(333);
         stk.Push("sqlserver");
+        Console.WriteLine("StackData :: " + stk.Pop());
         foreach (object o in stk)
         {
-            Console.WriteLine("Stack Data :: " + o);
+            Console.WriteLine("StackData :: " + o);
         }
         #endregion Stack
 
@@ -99,9 +104,10 @@ class CollectionsAndGenericTesting
         q.Enqueue(DateTime.Parse("8-oct-1985"));
         q.Enqueue(333);
         q.Enqueue("sqlserver");
+        Console.WriteLine("QueueData :: " + q.Dequeue());
         foreach (object o in q)
         {
-            Console.WriteLine("Queue Data :: " + o);
+            Console.WriteLine("QueueData :: " + o);
         }
         #endregion Queue
         #endregion Non-Generic-Collections
@@ -113,9 +119,10 @@ class CollectionsAndGenericTesting
         lst.Add(new Employee() { empId = 102, empName = "Name2" });
         lst.Add(new Employee() { empId = 103, empName = "Name3" });
         lst.Add(new Employee() { empId = 104, empName = "Name4" });
+        Console.WriteLine("ListData :: " + lst[1].empId);
         foreach (Employee i in lst)
         {
-            Console.WriteLine("List Data :: " + i.empId);
+            Console.WriteLine("ListData :: " + i.empId);
         }
         #endregion List
 
@@ -124,9 +131,11 @@ class CollectionsAndGenericTesting
         set.Add("Apple");
         set.Add("Banana");
         set.Add("Apple"); // Duplicate - will be ignored
+        List<string> setList = set.ToList();
+        Console.WriteLine("HashSetData :: " + setList[1]);
         foreach (var fruit in set)
         {
-            Console.WriteLine("HashSet Data :: " + fruit); // Order is not guaranteed
+            Console.WriteLine("HashSetData :: " + fruit); // Order is not guaranteed
         }
         #endregion HashSet
 
@@ -137,9 +146,10 @@ class CollectionsAndGenericTesting
         dct.Add(3, new Employee() { empId = 103, empName = "Name3" });
         dct.Add(2, new Employee() { empId = 102, empName = "Name2" });
         dct.Add(4, new Employee() { empId = 104, empName = "Name4" });
+        Console.WriteLine("DictionaryData :: " + dct[3].empId);
         foreach (KeyValuePair<int, Employee> kvp in dct)
         {
-            Console.WriteLine("Dictionary Data :: " + kvp.Key + " " + kvp.Value.empId);
+            Console.WriteLine("DictionaryData :: " + kvp.Key + " " + kvp.Value.empId);
         }
         #endregion Dictionary
 
@@ -151,9 +161,10 @@ class CollectionsAndGenericTesting
         sortedList.Add(3, new Employee() { empId = 103, empName = "Name3" });
         sortedList.Add(2, new Employee() { empId = 102, empName = "Name2" });
         sortedList.Add(4, new Employee() { empId = 104, empName = "Name4" });
+        Console.WriteLine("SortedListData :: " + sortedList[3].empId);
         foreach (KeyValuePair<int, Employee> kvp in sortedList)
         {
-            Console.WriteLine("SortedList Data :: " + kvp.Key + " " + kvp.Value.empId);
+            Console.WriteLine("SortedListData :: " + kvp.Key + " " + kvp.Value.empId);
         }
         #endregion SortedList
 
@@ -164,9 +175,10 @@ class CollectionsAndGenericTesting
         stack.Push(new Employee() { empId = 103, empName = "Name3" });
         stack.Push(new Employee() { empId = 102, empName = "Name2" });
         stack.Push(new Employee() { empId = 104, empName = "Name4" });
+        Console.WriteLine("StackData :: " + stack.Pop().empId);
         foreach (Employee s in stack)
         {
-            Console.WriteLine("Stack Data :: " + s.empId);
+            Console.WriteLine("StackData :: " + s.empId);
         }
         #endregion Stack
 
@@ -177,37 +189,46 @@ class CollectionsAndGenericTesting
         queue.Enqueue(new Employee() { empId = 103, empName = "Name3" });
         queue.Enqueue(new Employee() { empId = 102, empName = "Name2" });
         queue.Enqueue(new Employee() { empId = 104, empName = "Name4" });
+        Console.WriteLine("QueueData :: " + queue.Dequeue().empId);
         foreach (Employee s in queue)
         {
-            Console.WriteLine("Queue Data :: " + s.empId);
+            Console.WriteLine("QueueData :: " + s.empId);
         }
         #endregion Queue
 
         #region LinkedList
-        LinkedList<int> list = new LinkedList<int>();
-        list.AddLast(10);   // Add at the end
-        list.AddLast(20);
-        list.AddFirst(5);   // Add at the beginning
+        LinkedList<int> linkedList = new LinkedList<int>();
+        linkedList.AddLast(100);   // Add at the end
+        linkedList.AddLast(20);
+        linkedList.AddFirst(5);   // Add at the beginning
+        Console.WriteLine("LinkedListData :: " + linkedList.ToList()[2]);
         Console.WriteLine("LinkedList after adding 5, 10, 20:");
-        foreach (int number in list)
+        foreach (int number in linkedList)
         {
-            Console.WriteLine("LinkedList Data :: " + number);
+            Console.WriteLine("LinkedListData :: " + number);
         }
         // Insert after a specific node
-        LinkedListNode<int>? node = list.Find(10);
+        LinkedListNode<int>? node = linkedList.Find(100);
         if (node != null)
         {
-            list.AddAfter(node, 15);
+            linkedList.AddAfter(node, 101);
         }
-        Console.WriteLine("\nLinkedList After inserting 15 after 10:");
-        foreach (int number in list)
+        Console.WriteLine("LinkedList After inserting 101 after 101:");
+        foreach (int number in linkedList)
         {
-            Console.WriteLine("LinkedList Data :: " + number);
+            Console.WriteLine("LinkedListData :: " + number);
         }
-        #endregion LinkedList
+#endregion LinkedList
         #endregion Generic-Collections
 
-   }
+
+
+class Employee
+    {
+        public int empId { set; get; }
+        public string? empName { set; get; }
+    }
+}
 }
 
 class Employee
@@ -218,54 +239,62 @@ class Employee
 
 // OutPut
 /*
-ArrayList Data :: Testing
-ArrayList Data :: 7
-ArrayList Data :: 10/8/1985 12:00:00AM
-Hashtable Data :: vb 333
-Hashtable Data :: cs cs.net
-Hashtable Data :: asp asp.net
-Hashtable Data :: ora 10/8/1985 12:00:00AM
-SortedList Data :: asp asp.net
-SortedList Data :: cs cs.net
-SortedList Data :: ora 10/8/1985 12:00:00AM
-SortedList Data :: vb 333
-Stack Data :: sqlserver
-Stack Data :: 333
-Stack Data :: 10/8/1985 12:00:00AM
-Stack Data :: cs.net
-Queue Data :: cs.net
-Queue Data :: 10/8/1985 12:00:00AM
-Queue Data :: 333
-Queue Data :: sqlserver
-List Data :: 101
-List Data :: 102
-List Data :: 103
-List Data :: 104
-HashSet Data :: Apple
-HashSet Data :: Banana
-Dictionary Data :: 1 101
-Dictionary Data :: 3 103
-Dictionary Data :: 2 102
-Dictionary Data :: 4 104
-SortedList Data :: 1 101
-SortedList Data :: 2 102
-SortedList Data :: 3 103
-SortedList Data :: 4 104
-Stack Data :: 104
-Stack Data :: 102
-Stack Data :: 103
-Stack Data :: 101
-Queue Data :: 101
-Queue Data :: 103
-Queue Data :: 102
-Queue Data :: 104
+ArrayListData :: Testing
+ArrayListData :: Testing
+ArrayListData :: 7
+ArrayListData :: 10/8/1985 12:00:00AM
+HashtableData :: cs.net
+HashtableData :: ora 10/8/1985 12:00:00AM
+HashtableData :: asp asp.net
+HashtableData :: cs cs.net
+HashtableData :: vb 333
+SortedListData :: asp.net
+SortedListData :: asp asp.net
+SortedListData :: cs cs.net
+SortedListData :: ora 10/8/1985 12:00:00AM
+SortedListData :: vb 333
+StackData :: sqlserver
+StackData :: 333
+StackData :: 10/8/1985 12:00:00AM
+StackData :: cs.net
+QueueData :: cs.net
+QueueData :: 10/8/1985 12:00:00AM
+QueueData :: 333
+QueueData :: sqlserver
+ListData :: 102
+ListData :: 101
+ListData :: 102
+ListData :: 103
+ListData :: 104
+HashSetData :: Banana
+HashSetData :: Apple
+HashSetData :: Banana
+DictionaryData :: 103
+DictionaryData :: 1 101
+DictionaryData :: 3 103
+DictionaryData :: 2 102
+DictionaryData :: 4 104
+SortedListData :: 103
+SortedListData :: 1 101
+SortedListData :: 2 102
+SortedListData :: 3 103
+SortedListData :: 4 104
+StackData :: 104
+StackData :: 102
+StackData :: 103
+StackData :: 101
+QueueData :: 101
+QueueData :: 103
+QueueData :: 102
+QueueData :: 104
+LinkedListData :: 20
 LinkedList after adding 5, 10, 20:
-LinkedList Data :: 5
-LinkedList Data :: 10
-LinkedList Data :: 20
-After inserting 15 after 10:
-LinkedList Data :: 5
-LinkedList Data :: 10
-LinkedList Data :: 15
-LinkedList Data :: 20
+LinkedListData :: 5
+LinkedListData :: 100
+LinkedListData :: 20
+LinkedList After inserting 101 after 100:
+LinkedListData :: 5
+LinkedListData :: 100
+LinkedListData :: 101
+LinkedListData :: 20
 */
